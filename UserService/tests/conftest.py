fae93b0857cb03d6ad00941bd045a1c198963769
Leizle <ho.leizle@gmail.com>
2020-03-26 13:08:34 -0700

    deleted unused codes, updated db config

--- UserService/tests/conftest.py
+++ UserService/tests/conftest.py
@@ -2,20 +2,6 @@ import pytest
 import dotenv
 import os
 from users.app import create_app
-# from users.db import db
-
-
-# dotenv.load_dotenv()
-# db_params = {
-#     'host': os.environ['POSTGRES_HOST_TEST'],
-#     'database': os.environ['POSTGRES_DB_TEST'],
-#     'user': os.environ['POSTGRES_USER_TEST'],
-#     'pwd': os.environ['POSTGRES_PASSWORD_TEST'],
-#     'port': os.environ['POSTGRES_PORT_TEST'],
-# }
-
-# DB_URI = 'postgresql://{user}@{host}:{port}/{database}'
-
 
 @pytest.fixture
 def app():
